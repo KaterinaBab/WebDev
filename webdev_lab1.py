18fdb3ea090437fafe4f4654e7d513a0eef89d14
KaterinaBab <johndoe@example.co>
2024-02-26 12:32:25 +0200

    repo

--- webdev_lab1.py
+++ webdev_lab1.py
@@ -10,9 +10,30 @@ def more(text):
             if reply == 'n':
                 break
 
-url = 'http://google.com/'  # προσδιορισμός του url
+url = input('Enter a URL:') # προσδιορισμός του url
+if not url.startswith("http://"):
+    url = "http://" + url
 
 with requests.get(url) as response:  # το αντικείμενο response
     #html = response.text
     #more(html)
+
+    print(f"Website headers are {url} \n , {response.headers} \n\n")
+
+    server = response.headers.get('Server')
+
+    if server:
+        print(f"The server is {server}")
+    else:
+        print("No server found")
     
+    cookies = response.cookies.get_dict()
+
+    print(cookies)
+    cookie_header = response.headers.get('Set-cookie')
+
+    if cookie_header:
+        print(f"The cookie is{cookie_header}")
+    else:
+        print("This website does not use cookies.")
+
